386ac7110f0f51ee94bfbafd5568e6fd34d5ea0f
Alex_Smirnov <95142946+R1ng3R@users.noreply.github.com>
2024-08-23 12:58:44 +0300

    Add files via upload

--- corn.py
+++ corn.py
@@ -1,28 +1,10 @@
 import cv2
-from functions import *
 import numpy as np
+from functions import *
 
-for i in range(7):
-    path = 'data/' + str(i+1) + '.png'
-
-    img_bgr = cv2.imread(path)
-    img = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB) # Перевод и bgr в rgb
-    # x = float(input('Введите параметр x_distortion:\n'))
-    # y = float(input('Введите параметр y_distortion:\n'))
-
-    x, y = 0.2, 0.2
-
-    # Запуск функции:
-    output_img = distortion(img, x, y)
-
-    # Отображение результатов:
-    plot_result(img, output_img)
-
-    # запуск окна настроек
-    interctive_window(path, itr=i)
-
+# Load the two images
 img1 = cv2.imread('1.png')
-img2 = cv2.imread(interctive_window(output_img))
+img2 = cv2.imread(interctive_window())
 
 # Convert the images to grayscale
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -47,3 +29,12 @@ num_corners2 = len(corner_points2[0])
 similarity = min(num_corners1, num_corners2) / max(num_corners1, num_corners2)
 
 print('Similarity:', similarity)
+
+# Visualize the corner points
+# cv2.drawChessboardCorners(img1, (7, 7), corner_points1, True)
+# cv2.drawChessboardCorners(img2, (7, 7), corner_points2, True)
+#
+# cv2.imshow('Image 1', img1)
+# cv2.imshow('Image 2', img2)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
